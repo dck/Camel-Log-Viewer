ee281c601f2a04a54f59b0c05331ad9f511a072b
Denis Usanov <usanovdd@gmail.com>
2011-10-21 02:07:23 +0400

    Added sketch of the script

---
+++ CamelLogView.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+
+my $script_name = "Camel Log Viewer";
+my $script_version = "0.1";
+my $script_description = "The script shows logs from channel in separate tab";
+my $theCommand = "log";
+
+Xchat::register($script_name, $script_version, $script_description);
+
+Xchat::print "\n\n\00312.::    $script_name    ::.\003\n";
+Xchat::print "\00312:::    Version $script_version  :::\003\n";
+Xchat::print "\00312:::    © Deck    :::\003\n\n";
+
+
+my $options = { 
+                data      => undef,
+                priority  => Xchat::PRI_NORM,
+                help_text => "This command creates new tab and load the lof of current channel there"                
+            };
+
+Xchat::hook_command( $theCommand, \&cbTheCommand, $options );
+
+sub cbTheCommand
+{
+    Xchat::print ("Hello");
+    return Xchat::EAT_ALL;
+}
+
